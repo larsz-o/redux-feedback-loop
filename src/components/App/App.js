02afe57fa6ca91eeeea2a3aaccb61bf1b8d97eaa
Lars <larsmackenzie@gmail.com>
2018-09-07 16:03:21 -0500

    installed react-router-dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+//import all of the views here too. 
+import {HashRouter as Router, Route} from "react-router-dom";
 
 class App extends Component {
   render() {
