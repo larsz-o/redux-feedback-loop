13451f93f6a68619226a24ca598871d431235b15
Lars <larsmackenzie@gmail.com>
2018-09-07 16:41:55 -0500

    setup reducers and redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, combineReducers, applyMiddleware} from 'redux'; 
+import {Provider} from 'react-redux'; 
+import logger from 'redux-logger'; 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const dinnerLog = (state = '', action) => {
+    return state; 
+} // stores the current dinner entry 
+
+const newFeedback = {
+    taste: 0, 
+    texture: 0,
+    creativity: 0, 
+    nutrition: 0, 
+    comments: '', 
+    flagged: false, 
+    date: ''
+};
+
+const feedback = (state = newFeedback, action ) => {
+    return state; 
+}
+const storeInstance = createStore (
+    combineReducers({
+        dinnerLog,
+        feedback
+    })
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
