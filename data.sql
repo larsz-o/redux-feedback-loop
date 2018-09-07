15f655206ef5b86c085aacf58e21e59af0cac3a2
Lars <larsmackenzie@gmail.com>
2018-09-07 15:43:41 -0500

    edited database structure

--- data.sql
+++ data.sql
@@ -1,18 +1,19 @@
 -- Database should be prime_feedback
-CREATE DATABASE "prime_feedback";
+CREATE DATABASE "dinner_diary";
 
 -- Switch to "prime_feedback" before making:
 -- Table to store the feedback
 CREATE TABLE "feedback" (
   "id" serial primary key,
-  "feeling" INT not null,
-  "understanding" INT not null,
-  "support" INT not null,
+  "taste" INT not null,
+  "texture" INT not null,
+  "creativity" INT not null,
+  "nutrition" INT not null,
   "comments" text,
   "flagged" boolean default false,
   "date" date not null default CURRENT_DATE
 ); 
 
 -- Sample feedback item
-INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
-VALUES (4, 4, 5, 'Doing Great!');
+INSERT INTO "feedback" ("taste", "texture", "creativity", "nutrition", "comments")
+VALUES (4, 4, 5, 3, 'Doing Great!');
