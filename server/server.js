1738774d3b0f1e54320166f36bb3b7436f8024c7
Lars <larsmackenzie@gmail.com>
2018-09-09 11:14:25 -0500

    reconfig server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.static('build'));
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('client/build'));
+    app.use(express.static('build'));
   
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
