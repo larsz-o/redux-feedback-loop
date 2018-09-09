44e8e83324668c18e492f052486cdba11bdadc54
Lars <larsmackenzie@gmail.com>
2018-09-09 11:12:50 -0500

    reconfig server.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,16 @@ app.use(bodyParser.json()); // needed for angular requests
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+  }
 /** ---------- EXPRESS ROUTES ---------- **/
 const feedbackRouter = require('./routes/feedback.router.js'); 
 app.use('/feedback', feedbackRouter); 
